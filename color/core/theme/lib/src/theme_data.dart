268ff7781d7df2b884df62f034c4ac9b89145fe1
gasigogi <sh9306@gmail.com>
2024-03-25 19:47:32 +0900

    update

---
+++ color/core/theme/lib/src/theme_data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+abstract class ICoreThemeData {
+  final ICoreTextTheme textTheme;
+  final ICoreColorScheme colorScheme;
+
+  const ICoreThemeData({
+    required this.textTheme,
+    required this.colorScheme,
+  });
+}
+
+abstract class ICoreTextTheme {
+  const ICoreTextTheme();
+}
+
+abstract class ICoreColorScheme {
+  final Brightness brightness;
+
+  const ICoreColorScheme({
+    required this.brightness,
+  });
+}
